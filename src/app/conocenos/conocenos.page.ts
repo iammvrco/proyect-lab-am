2497261831b420e37625e9c763d0b216be7f6f53
zDarkZero <zeroo_tobias@hotmail.com>
2020-06-09 02:42:21 -0500

    Se agrego el slides en Home

--- src/app/conocenos/conocenos.page.ts
+++ src/app/conocenos/conocenos.page.ts
@@ -13,10 +13,16 @@ export class ConocenosPage implements OnInit {
     nombre: '',
     descripcion:'',
     audio:'',
+    dirr:'',
+    tel:'',
+    correo:'',
     marker:'',
     lon:0,
     lat:0
   }
+
+  logo;
+
   mapa: mapboxgl.Map;
   marker;
   constructor(private activatedRoute: ActivatedRoute) { }
@@ -24,16 +30,24 @@ export class ConocenosPage implements OnInit {
   ngOnInit() {
     this.informacion.nombre=this.activatedRoute.snapshot.paramMap.get('sembradio');
     if(this.informacion.nombre==="Los Morales"){
-      this.informacion.descripcion="Aqui se venden frutas.";
+      this.informacion.descripcion="El cultivo Los Morales utiliza técinas de regado avanzadas para garantizar que las frutas que cultivamos aquí estén bien jugosas, también cuidamos mucho el los nutrientes que les damos para obtener unas cosechas más dulces.";
+      this.informacion.dirr="Montemorelos, N.L.";
+      this.informacion.tel="8123431354";
+      this.informacion.correo="GranjaMZ@gmail.com"
       this.informacion.marker='./assets/marker/icon-morales.png';
       this.informacion.lon=-100.035;
       this.informacion.lat=25.241;
+      this.logo="./assets/logo/los-morales.png";
     }
     else{
-      this.informacion.descripcion="Aquí se venden verduras.";
+      this.informacion.descripcion="El cultivo Los Zambrano está repartido por partes iguales asignando los cultivos de verduras a zonas en particulares debido a los cuidados epeciales que necesitan utilizando abono de la mejor calidad y un control de plagas natural.";
+      this.informacion.dirr="Montemorelos, N.L.";
+      this.informacion.tel="8123431354";
+      this.informacion.correo="GranjaMZ@gmail.com";
       this.informacion.marker='./assets/marker/icon-zambrano.png',
       this.informacion.lon=-100.088;
       this.informacion.lat=25.220;
+      this.logo="./assets/logo/los-zambrano.png";
     }
     mapboxgl.accessToken = environment.mapboxkey;
     this.mapa = new mapboxgl.Map({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -22,4 +22,16 @@ export class HomePage {
     },
   ];
 
+  descripciones = [
+    "1.-ELECCION DE SEMILLAS Poner las semillas dentro de un vaso con agua y removemos. Las que se hundan son las buenas y las que floten seguramente no sean viables para sembrar.Pero aún hundiéndose, podrían no germinar si se han conservado mal, son muy viejas o han sido sobrecalentadas, por ejemplo.",
+    "2.-PREPARACION DEL TERRENO En el primer caso lo que realizamos es retirar las malezas, piedras y palos que pudiera haber en la tierra y la cavar con una azada. Aportamos un poco de abono para semilleros al suelo. Romper los terrones y pasar un rastrillo o cultivador para hacer la tierra más fina y esponjosa. Si la tierra está muy seca, la regar hasta que se humedezca bien tanto en superficie como a unos centímetros de profundidad.",
+    "3.-DEPOSITO DE SEMILLAS Lo realizamos en pequeños hoyos, surcos o distribuidas uniformemente por el terreno, esta egún sea el tamaño de la semilla y el tipo de planta será necesario uno de éstos tipos de siembra.",
+    "4.-CUBRIR LAS SEMILLAS Cubrimos con una capa de tierra de aproximadamente dos veces el tamaño de la semilla. Es decir, una semilla de 2 mm basta con enterrarla 4 mm en el terreno. Cuanto mayor es la semilla, más se podrá enterrar.",
+    "5.-REGADO Con una regadera de lluvia fina y mucho mejor si el agua que utilizamos no contiene cloro, Sobre todo si las semillas son muy pequeñas ya que regando entran mejor en contacto con la tierra; Para las semillas grandes no es necesario que la regemos siempre y cuando la tierra ya estuviera húmeda.",
+    "6.-CONTROL DE TEMPERATURA Y HUMEDAD Si hace mucho sol y calor, tapamos con una malla de sombreo o similar, que deje pasar parcialmente la luz pero no de forma directa. Por el contrario, si hace demasiado frío para que las semillas germinen, cubrimos con un plástico transparente a modo de invernadero. Esparcir un poco de mantillo o turba negra sobre el suelo también ayudará a subir la temperatura de éste siempre y cuando haga sol. Una cama caliente es otro buen método para aumentar la temperatura del suelo.",
+    "7.-RECOLECCION Recolectamos los productos de forma manual despues de que ah terminado su proceso de maduracion, la recoleccion mecánica no es posible. Para esto, a menudo se usa un machete o un cuchillo especializado.",
+    "8.-LIMPIEZA Y SECADO Limpiamos de manera manual cuidando cada producto despues de esto, están listos para secarse. Este es otro paso crucial en la mejora del sabor",
+    "9.-ALMACENAMIENTO Mantenemos el oxígeno fuera y aseguramos que los niveles de humedad sean consistentes utilizando bolsas GrainPro hermética de esta manera lo almacenamos hasta que el comprador pase a recogerlo."
+  ];
+
 }

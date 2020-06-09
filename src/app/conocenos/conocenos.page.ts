4a9fed1bfcd63c88481be9d391ff20eba14625fd
zDarkZero <zeroo_tobias@hotmail.com>
2020-06-09 01:17:29 -0500

    Se agrego marcador en el mapa

--- src/app/conocenos/conocenos.page.ts
+++ src/app/conocenos/conocenos.page.ts
@@ -13,35 +13,42 @@ export class ConocenosPage implements OnInit {
     nombre: '',
     descripcion:'',
     audio:'',
-    marcador:[0,0]
+    marker:'',
+    lon:0,
+    lat:0
   }
   mapa: mapboxgl.Map;
-
+  marker;
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.informacion.nombre=this.activatedRoute.snapshot.paramMap.get('sembradio');
     if(this.informacion.nombre==="Los Morales"){
       this.informacion.descripcion="Aqui se venden frutas.";
-      this.informacion.marcador=[-100.117, 25.4167];
+      this.informacion.marker='./assets/marker/icon-morales.png';
+      this.informacion.lon=-100.035;
+      this.informacion.lat=25.241;
     }
     else{
-      this.informacion.descripcion="Aquí se venden verduras."
-      this.informacion.marcador=[-100.117, 29.167];
+      this.informacion.descripcion="Aquí se venden verduras.";
+      this.informacion.marker='./assets/marker/icon-zambrano.png',
+      this.informacion.lon=-100.088;
+      this.informacion.lat=25.220;
     }
     mapboxgl.accessToken = environment.mapboxkey;
     this.mapa = new mapboxgl.Map({
     container: 'mapabox', // container id
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: this.informacion.marcador, // starting position
-    zoom: 12 // starting zoom
-    });
-    this.mapa.addControl(new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true
-      },
-      trackUserLocation: true,
-      showUserLocation:true
-    }));
+    center: [this.informacion.lon,this.informacion.lat], // starting position
+    zoom: 7.5 // starting zoom
+    }); 
+    this.crearMarcador();
+  }
+  crearMarcador(){
+    var el=document.createElement('img');
+    el.src=this.informacion.marker;
+    el.style.width='50px';
+    el.style.height='50px';
+    const marker=new mapboxgl.Marker(el).setLngLat([this.informacion.lon, this.informacion.lat]).addTo(this.mapa);
   }
 }
